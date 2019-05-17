5319009a81e133877e9d420ab30b0357554f7aac
Asish Mehta <asish.mehta@kcl.ac.uk>
2019-05-17 22:58:22 +0100

    Finished Coding Challenge

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-#passfort coding task
+
+In order to run the file, you will need Python and Flask. To run the application the first command you will need to enter in terminal once entering the directory is:
+
+$ export FLASK_APP=main.py 
+
+The second command to actually start the Flask server is:
+
+$ flask run
+
+From there you should see the following to show the server is running:
+
+
+[{'dates': ['2012-01-05', '2012-01-06', '2012-01-07'], 'documents': ['This is the first revision of the wiki for setting up api', 'second revision', 'third revision'], 'title': 'API'}, {'dates': ['2012-01-05', '2012-01-06', '2012-01-07'], 'documents': ['this is the first revision for setting up database wiki', 'the second', '2third'], 'title': 'database'}]
+ * Serving Flask app "main"
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+
+
+I have included some test data to test the API methods I have created. The test data looks as follows:
+
+example = {
+        'title':'API',
+        'documents':["This is the first revision of the wiki for setting up api","second revision","third revision"],
+        'dates': ["2012-01-05","2012-01-06","2012-01-07"]
+    }
+
+example2 = {
+        'title':'database',
+        'documents':["this is the first revision for setting up database wiki","the second","2third"],
+        'dates': ["2012-01-05","2012-01-06","2012-01-07"]
+    }
+
+
+I have included example URL's for each of the API's.
+
+GET /documents --> http://127.0.0.1:5000/documents
+GET /documents/<title> --> http://127.0.0.1:5000/documents/API or http://127.0.0.1:5000/documents/database
+GET /documents/<title>/latest --> http://127.0.0.1:5000/documents/database/latest
+GET /documents/<title>/<timestamp> --> http://127.0.0.1:5000/documents/database/2012-01-06
+
+The post method for /documents is implemented and works but at this point I was close to 3 hours and wanted to stick to the time constrait, which is why there is no unit testing. If you would like to see unit testing implemented, this is something I am more than happy to do, I just wanted to stay true to the time constraint. Please let me know if you need anything else.
+
+
--- main.py
+++ main.py
@@ -1,11 +1,115 @@
 from flask import Flask
+from flask import request
+from flask import json
+from flask import Response
+from datetime import date
 app = Flask(__name__)
+
+
+data = []
+example = {
+    	'title':'API',
+        'documents':["This is the first revision of the wiki for setting up api","second revision","third revision"],
+        'dates': ["2012-01-05","2012-01-06","2012-01-07"]
+    }
+
+example2 = {
+        'title':'database',
+        'documents':["this is the first revision for setting up database wiki","the second","2third"],
+        'dates': ["2012-01-05","2012-01-06","2012-01-07"]
+    }   
+ 
+data.append(example)
+data.append(example2)
+print(data)
 
 @app.route('/')
 def hello_world():
 	return 'Hello, World!'
 
+@app.route('/documents')
+def api_documents():
+	
+	data_titles=[]
+	for i in range(len(data)):
+		data_titles.append({'document_title':data[i]['title']})
+	js = json.dumps(data)
+	js2 = json.dumps(data_titles)
+	resp = Response(js2, status=200, mimetype='application/json')
+	
+	return resp
+
+@app.route('/documents/<title>',methods=['GET','PUT'])
+def api_title(title):
+	data_titles=[]
+	for i in range(len(data)):
+		data_titles.append(data[i]['title'])
+	if request.method == 'GET':
+		if any(title in s for s in data_titles):
+			number = data_titles.index(title)
+			js = data[number]['documents']
+			js3 = json.dumps(js)
+			resp = Response(js3, status = 200, mimetype='application/json')
+			return resp	
+		else:
+			resp = Response(response={'response':'error not found'},status = 400, mimetype='application/json')
+			return resp 
+	elif request.method == 'POST':
+		if any(title in s for s in data_titles):
+			number = data_titles.index(title)
+			new_data = request.json
+			data[number]['documents'].append(new_data['content'])
+			data[number]['dates'].append(str(date.today()))
+		else:
+			resp = Response(response={'response':'title not found'},status = 400, mimetype='application    /json')
+                      	return resp
+
+@app.route('/documents/<title>/latest')
+def api_title_latest(title):
+	data_titles=[]
+        for i in range(len(data)):
+                data_titles.append(data[i]['title'])
+        if request.method == 'GET':
+                if any(title in s for s in data_titles):
+                        number = data_titles.index(title)
+			latest =len(data[number]['documents'])-1
+                        js = data[number]['documents'][latest]
+                        js3 = json.dumps(js)
+                        resp = Response(js3, status = 200, mimetype='application/json')
+                        return resp    
+                else:
+			 resp = Response(response={'response':'error not found'},status = 400, mimetype='application/json')
+                 	 return resp
+
+
+@app.route('/documents/<title>/<timestamp>')
+def api_timestamp(title,timestamp):
+	data_titles=[]
+        for i in range(len(data)):
+                data_titles.append(data[i]['title'])
+	if any(title in s for s in data_titles):
+		revision_dates = []
+		title_index = data_titles.index(title)
+	
+		for i in range(len(data[title_index]['dates'])):
+			revision_dates.append(data[title_index]['dates'][i])
+		print(revision_dates)
+		if any(timestamp in j for j in revision_dates):
+			date_index = revision_dates.index(timestamp)
+			js = data[title_index]['documents'][date_index]
+			js4 = json.dumps(js)
+			resp = Response(js4, status = 200, mimetype='application/json')
+                        return resp
+		
+		else:
+
+			 resp = Response(response={'response':'that timestamp not found'},status = 400, mimetype='application/json')
+                         return resp
+		
+	else:
+		 resp = Response(response={'response':'title not found'},status = 400, mimetype='application/json')
+                 return resp
 
 @app.route('/test_page')
 def test_page_fn():
-	return 'Hello there'
+	return type(data)
